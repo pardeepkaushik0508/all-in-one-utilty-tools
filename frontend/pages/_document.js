03d60f1ef9b9b55ccc6789bc7e7a2cadc689eb3b
Pardeep <pardeep.luminoguru@gmail.com>
2026-06-10 13:29:15 +0530

    Addsense

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en" data-theme="light">
-      <Head />
+      <Head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5397189085296638"
+          crossOrigin="anonymous"
+        />
+      </Head>
       <body>
         <script
           dangerouslySetInnerHTML={{
